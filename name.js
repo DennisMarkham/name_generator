c483c0549772b8dd32da3f7fa585a1828f25e531
unknown <workdennis@yahoo.com>
2019-09-03 20:48:02 -0500

    kljlkj

--- name.js
+++ name.js
@@ -2,9 +2,32 @@
 // fs is a core Node package for reading and writing files
 var fs = require("fs");
 
-// This block of code will read from the "movies.txt" file.
-// It's important to include the "utf8" parameter or the code will provide stream data (garbage)
-// The code will store the contents of the reading inside the variable "data"
+var input = process.argv;
+var inputSpliced = input.splice(0,2);
+var newNames = "";
+
+
+
+var i;
+for (i = 0; i < input.length; i++) { 
+  newNames += input[i] + ", ";
+}
+
+
+
+fs.appendFile("surnames.txt", newNames, function(err) {
+
+  // If the code experiences any errors it will log the error to the console.
+  if (err) {
+    return console.log(err);
+  }
+
+  // Otherwise, it will print: "movies.txt was updated!"
+  console.log("movies.txt was updated!");
+
+});
+//OH SHIT, write is not the same as append
+
 fs.readFile("surnames.txt", "utf8", function(error, data) {
 
   // If the code experiences any errors it will log the error to the console.
@@ -24,5 +47,7 @@ fs.readFile("surnames.txt", "utf8", function(error, data) {
   var rn = Math.floor(Math.random() * dataArr.length);
 
   console.log(dataArr[rn]);
+
+  console.log(newNames);
 
 });
